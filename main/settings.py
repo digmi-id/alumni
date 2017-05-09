41e18b76a78627c24847d5f523c78fb92aea9518
imamdigmi <imam.digmi@gmail.com>
2017-05-09 16:16:38 +0700

    Fix error settings

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
+    'job',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'alumni.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
